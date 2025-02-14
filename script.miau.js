6f370557385dc8dd29f1f92eb148ebbbfac5a462
khadi00 <khokhaleila@gmail.com>
2025-02-14 02:52:18 +0100

    Delete script.miau.js

--- script.miau.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const heading = document.querySelector('h1');
-    const message = document.getElementById('message');
-
-    heading.addEventListener('click', () => {
-        heading.classList.add('hidden');
-        message.classList.remove('hidden');
-    });
-});
